1afaf8e3372f1227084481c0bbff06067b733538
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2022-03-10 10:38:58 -0500

    Initial Structure for expensableCLI

---
+++ expensableCLI.rb
@@ -0,0 +1,69 @@
+require_relative "sessions"
+
+class ExpensableCLI
+  def initialize
+    @user = nil
+    @notes = []
+  end
+
+  def intro
+    puts "####################################"
+    puts "#       Welcome to Expensable      #"
+    puts "####################################"
+  end
+
+  def main_menu
+    intro
+    action = ""
+
+    until action == "exit"
+      action = get_with_options(["login", "create_user", "exit"])
+      begin
+        case action
+        when "login"
+          login
+        when "create_user"
+          puts "execute create user"
+        when "exit"
+          puts "Thanks"
+        end
+      rescue HTTParty::ResponseError => error
+        parsed_error = JSON.parse(error.message, symbolize_names: true)
+        puts parsed_error
+      end
+    end
+  end
+
+  def get_with_options(options)
+    input = ""
+
+    loop do
+      puts options.join (" | ")
+      print "> "
+      input = gets.chomp
+      break if options.include?(input)
+      puts "Invalid option"
+    end
+    input
+
+  end
+
+  def login
+    credentials = login_form
+    @user = Sessions.login(credentials)
+  end
+
+  def login_form
+    print "username: "
+    username = gets.chomp
+
+    print "password: "
+    password = gets.chomp
+
+    { email: username, password: password }
+  end
+
+end
+
+prueba = Expensable.new
+prueba.main_menu
---
+++ modules/helpers.rb
@@ -0,0 +1,5 @@
+module Helpers.rb
+ def print_table(title, headings, rows)
+  # table = Terminal::Table.new
+  # table.title = title
+end
